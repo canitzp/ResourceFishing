8e4b9fc78aa09c0e225025ea61961739e03211bc
canitzp <canitzp@googlemail.com>
2018-09-28 08:11:02 +0200

    Initial commit

---
+++ src/main/java/de/canitzp/resourcefishing/ResourceFishing.java
@@ -0,0 +1,12 @@
+package de.canitzp.resourcefishing;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = ResourceFishing.MODID, name = ResourceFishing.MODNAME, version = ResourceFishing.MODVERSION)
+public class ResourceFishing{
+    
+    public static final String MODID = "resourcefishing";
+    public static final String MODNAME = "ResourceFishing";
+    public static final String MODVERSION = "@Version@";
+    
+}
